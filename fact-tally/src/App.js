7fc9d97f68f5964fbbcf4b30bdc683d0b9650612
vekoada <adam@vekony.com>
2023-09-02 11:40:20 -0700

    Create React app

---
+++ fact-tally/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <header class="header">
+      <div class="logo">
+        <img src="logo.png" alt="Today I Learned Logo"></img>
+        <h1>Today I Learned</h1>
+      </div>
+
+      <button class="btn btn-large btn-open">Share a fact</button>
+    </header>
+  );
+}
+
+export default App;
