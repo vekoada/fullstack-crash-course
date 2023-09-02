8cf33d24329f37bea07781b95d1aa8ab1ff542cb
vekoada <adam@vekony.com>
2023-09-02 10:29:59 -0700

    Remove demo facts, add dynamic category colors

--- starter-files/script.js
+++ starter-files/script.js
@@ -1,36 +1,13 @@
-// From data.js - we will dynamically add these below
-const initialFacts = [
-  {
-    id: 1,
-    text: "React is being developed by Meta (formerly facebook)",
-    source: "https://opensource.fb.com/",
-    category: "technology",
-    votesInteresting: 24,
-    votesMindblowing: 9,
-    votesFalse: 4,
-    createdIn: 2021,
-  },
-  {
-    id: 2,
-    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-    source:
-      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-    category: "society",
-    votesInteresting: 11,
-    votesMindblowing: 2,
-    votesFalse: 0,
-    createdIn: 2019,
-  },
-  {
-    id: 3,
-    text: "Lisbon is the capital of Portugal",
-    source: "https://en.wikipedia.org/wiki/Lisbon",
-    category: "society",
-    votesInteresting: 8,
-    votesMindblowing: 3,
-    votesFalse: 1,
-    createdIn: 2015,
-  },
+//Adding category colors from data.js
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
 ];
 
 //Select DOM elements
@@ -54,6 +31,7 @@ async function loadFacts() {
     }
   );
   const data = await res.json(); //Obtaining json data
+  //const filteredData = data.filter((fact) => fact.category === "society"); Demonstration of filter function
   createFactsList(data);
 }
 
@@ -70,8 +48,9 @@ function createFactsList(dataArray) {
       target="_blank"
       >(Source)</a>
       </p>
-      <span class="tag" style="background-color:
-      #3b82f6">
+      <span class="tag" style="background-color: ${
+        CATEGORIES.find((cat) => cat.name === fact.category).color
+      }">
         ${fact.category}
       </span>
     </li>`
@@ -79,8 +58,6 @@ function createFactsList(dataArray) {
   const html = htmlArr.join("");
   factsList.insertAdjacentHTML("afterbegin", html);
 }
-
-createFactsList(initialFacts);
 
 //Toggle form visibility
 btn.addEventListener("click", function () {
