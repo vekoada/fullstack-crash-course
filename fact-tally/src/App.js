04b9abc3ef2ccf2dcba4ac5d9ddfc6234233a02a
vekoada <adam@vekony.com>
2023-09-02 11:48:22 -0700

    Copy font links to public index, import style.css to App

--- fact-tally/src/App.js
+++ fact-tally/src/App.js
@@ -1,12 +1,14 @@
+import "./style.css";
+
 function App() {
   return (
-    <header class="header">
-      <div class="logo">
+    <header className="header">
+      <div className="logo">
         <img src="logo.png" alt="Today I Learned Logo"></img>
         <h1>Today I Learned</h1>
       </div>
 
-      <button class="btn btn-large btn-open">Share a fact</button>
+      <button className="btn btn-large btn-open">Share a fact</button>
     </header>
   );
 }
